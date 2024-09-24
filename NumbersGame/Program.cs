2552871e437c850a7649e69e0b6df42b22358164
HugoW5 <hugo05.wakt@gmail.com>
2024-09-24 15:41:33 +0200

    PrintPhrase() method & arrays

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -2,12 +2,25 @@
 {
     internal class Program
     {
+
+        static int[] closeNum = { 0, 20, 40, 60, 80, 100 };
+        //x --> !
+        static string[] closePhrases = {
+            "Väldigt Jätte nära!",
+            "Det var väldigt nära!",
+            "Det bränns!",
+            "Det var nära!",
+            "Långt ifrån!",
+            "Väldigt Långt ifrån",
+        };
+
         static void Main(string[] args)
         {
             Random rng = new Random();
             int number = rng.Next(1, 21);
             int tries = 5;
 
+            Console.WriteLine(number);
             Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
 
             while (tries > 0)
@@ -16,22 +29,32 @@
                 int guess = 0;
                 if (int.TryParse(Console.ReadLine(), out guess))
                 {
-                    if (guess < number)
-                    {
-                        Console.WriteLine("Tyvärr du gissade för lågt");
-                    }
-                    else if( guess > number)
-                    {
-                        Console.WriteLine("Tyvärr du gissade för högt!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Wohoo! Du gjorde det!");
-                    }
+                    PrintPhrase(guess, number);
                     tries--;
                 }
             }
-            Console.WriteLine("Tyvärr du lyckades inte gissa talet på fem försök!");
+            if (tries == 0)
+            {
+                Console.WriteLine("Tyvärr du lyckades inte gissa talet på fem försök!");
+            }
+        }
+
+
+        static void PrintPhrase(int guess, int number)
+        {
+
+            double close = ((number - guess) * 100) / number;
+            var nearest = closeNum.OrderBy(x => Math.Abs(x - close)).First();
+            int index = closeNum.ToList().IndexOf(nearest);
+            if (close < 1)
+            {
+                Console.WriteLine("För högt!");
+            }
+            else
+            {
+                Console.WriteLine(closePhrases[index]);
+            }
+
         }
     }
 }
