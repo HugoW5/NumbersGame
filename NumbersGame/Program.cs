6c414979b60731b328510fe39ed096a49c4c905c
HugoW5 <hugo05.wakt@gmail.com>
2024-09-24 18:09:25 +0200

    finished project

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -1,60 +1,68 @@
-﻿namespace NumbersGame
+﻿using System.Transactions;
+
+namespace NumbersGame
 {
-    internal class Program
-    {
+	internal class Program
+	{
 
-        static int[] closeNum = { 0, 20, 40, 60, 80, 100 };
-        //x --> !
-        static string[] closePhrases = {
-            "Väldigt Jätte nära!",
-            "Det var väldigt nära!",
-            "Det bränns!",
-            "Det var nära!",
-            "Långt ifrån!",
-            "Väldigt Långt ifrån",
-        };
+		static int[] closeNum = { 0, 20, 40, 60, 80, 100 };
+		//x --> 
+		static string[] closePhrases = {
+			"Väldigt Jätte nära!!",
+			"Det var väldigt nära!",
+			"Det bränns!",
+			"Det börjar närma sig!",
+			"Långt ifrån!",
+			"Väldigt Långt ifrån",
+		};
 
-        static void Main(string[] args)
-        {
-            Random rng = new Random();
-            int number = rng.Next(1, 21);
-            int tries = 5;
+		static int tries = 5;
 
-            Console.WriteLine(number);
-            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+		static void Main(string[] args)
+		{
+			Random rng = new Random();
+			int number = rng.Next(1, 21);
+			Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
 
-            while (tries > 0)
-            {
-                Console.Write("Gissning: ");
-                int guess = 0;
-                if (int.TryParse(Console.ReadLine(), out guess))
-                {
-                    PrintPhrase(guess, number);
-                    tries--;
-                }
-            }
-            if (tries == 0)
-            {
-                Console.WriteLine("Tyvärr du lyckades inte gissa talet på fem försök!");
-            }
-        }
+			while (tries > 0)
+			{
+				Console.Write("Gissning: ");
+				int guess = 0;
+				if (int.TryParse(Console.ReadLine(), out guess))
+				{
+					PrintPhrase(guess, number);
+					tries--;
+				}
+			}
+			if (tries == 0)
+			{
+				Console.WriteLine("Tyvärr du lyckades inte gissa talet på fem försök!");
+			}
+		}
 
+		static void PrintPhrase(int guess, int number)
+		{
+			if (guess == number)
+			{
+				Console.WriteLine("Wohoo! Du gjorde det!");
+				//make tries 0 to break while loop
+				tries = 0;
+				return;
+			}
 
-        static void PrintPhrase(int guess, int number)
-        {
+			double close = ((number - guess) * 100) / number;
+			var nearest = closeNum.OrderBy(x => Math.Abs(x - close)).First();
+			int index = closeNum.ToList().IndexOf(nearest);
 
-            double close = ((number - guess) * 100) / number;
-            var nearest = closeNum.OrderBy(x => Math.Abs(x - close)).First();
-            int index = closeNum.ToList().IndexOf(nearest);
-            if (close < 1)
-            {
-                Console.WriteLine("För högt!");
-            }
-            else
-            {
-                Console.WriteLine(closePhrases[index]);
-            }
+			if (close < 1)
+			{
+				Console.WriteLine("För högt!");
+			}
+			else
+			{
+				Console.WriteLine(closePhrases[index]);
+			}
 
-        }
-    }
+		}
+	}
 }
